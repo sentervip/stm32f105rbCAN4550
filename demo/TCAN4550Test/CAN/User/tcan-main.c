e7681311e90c8494644e0db41af6bb76ce683e53
sentervip <workervip@163.com>
2020-08-20 19:45:07 +0800

    release V1.0 实现ext ID frame 过滤收发10ms间隔持续发送8000 帧ok

--- demo/TCAN4550Test/CAN/User/tcan-main.c
+++ demo/TCAN4550Test/CAN/User/tcan-main.c
@@ -127,17 +127,10 @@ int main(void)
 	{
 		while(1);//SPIÕ®–≈ ß∞‹
 	}
-	
-
-
-	
 	/*********************************************
 	 * Everything at this point is for TCAN4550  *
 	 *********************************************/
 	Init_CAN();										// Run the main MCAN configuration sequence. The bulk of the configuration is in this!
-	
-
-
 	/* Define the CAN message we want to send*/
 	TCAN4x5x_MCAN_TX_Header header = {0};			// Remember to initialize to 0, or you'll get random garbage!
 	uint8_t data[8] = {0x55, 0x66, 0x77, 0x88};		// Define the data payload
@@ -202,7 +195,7 @@ int main(void)
 
 				// numBytes will have the number of bytes it transfered in it. Or you can decode the DLC value in MsgHeader.DLC
 				// The data is now in dataPayload[], and message specific information is in the MsgHeader struct.
-				if (MsgHeader.ID == 0x0201)		// Example of how you can do an action based off a received address
+				if (MsgHeader.ID == 0x00258003)		// 0x00258003 Example of how you can do an action based off a received address
 				{
 					if(dataPayload[0]==0x01)	//µ„¡¡LED
 					{
@@ -333,15 +326,13 @@ Init_CAN(void)
 
 	/* Configure the default CAN packet filtering settings */
 	TCAN4x5x_MCAN_Global_Filter_Configuration gfc = {0};
-	gfc.RRFE = 1;                                               // Reject remote frames (TCAN4x5x doesn't support this)
-	gfc.RRFS = 1;                                               // Reject remote frames (TCAN4x5x doesn't support this)
+	gfc.RRFE = 0;      //1                                         // Reject remote frames (TCAN4x5x doesn't support this)
+	gfc.RRFS = 0;      //1                                         // Reject remote frames (TCAN4x5x doesn't support this)
 	
-//	gfc.ANFE = TCAN4x5x_GFC_REJECT;                // Default behavior if incoming message doesn't match a filter is to reject for extended ID messages (29 bit IDs)
-//	gfc.ANFS = TCAN4x5x_GFC_REJECT;                // Default behavior if incoming message doesn't match a filter is to reject for standard ID messages (11 bit IDs)
-
-	
-	gfc.ANFE = TCAN4x5x_GFC_ACCEPT_INTO_RXFIFO0;                // Default behavior if incoming message doesn't match a filter is to accept into RXFIO0 for extended ID messages (29 bit IDs)
-	gfc.ANFS = TCAN4x5x_GFC_ACCEPT_INTO_RXFIFO0;                // Default behavior if incoming message doesn't match a filter is to accept into RXFIO0 for standard ID messages (11 bit IDs)
+	gfc.ANFE = TCAN4x5x_GFC_REJECT;                // Default behavior if incoming message doesn't match a filter is to reject for extended ID messages (29 bit IDs)
+	gfc.ANFS = TCAN4x5x_GFC_REJECT;                // Default behavior if incoming message doesn't match a filter is to reject for standard ID messages (11 bit IDs)
+//	gfc.ANFE = TCAN4x5x_GFC_ACCEPT_INTO_RXFIFO0;                // Default behavior if incoming message doesn't match a filter is to accept into RXFIO0 for extended ID messages (29 bit IDs)
+//	gfc.ANFS = TCAN4x5x_GFC_ACCEPT_INTO_RXFIFO0;                // Default behavior if incoming message doesn't match a filter is to accept into RXFIO0 for standard ID messages (11 bit IDs)
 
 	/* ************************************************************************
 	 * In the next configuration block, we will set the MCAN core up to have:
@@ -354,16 +345,16 @@ Init_CAN(void)
 	 *   - 2 Transmit buffers supporting up to 64 bytes of data payload
 	 */
 	TCAN4x5x_MRAM_Config MRAMConfiguration = {0};
-	MRAMConfiguration.SIDNumElements = 1;						// Standard ID number of elements, you MUST have a filter written to MRAM for each element defined
-	MRAMConfiguration.XIDNumElements = 1;						// Extended ID number of elements, you MUST have a filter written to MRAM for each element defined
-	MRAMConfiguration.Rx0NumElements = 64;						// RX0 Number of elements
+	MRAMConfiguration.SIDNumElements = 0;						// Standard ID number of elements, you MUST have a filter written to MRAM for each element defined
+	MRAMConfiguration.XIDNumElements = 8; //1 aizj						// Extended ID number of elements, you MUST have a filter written to MRAM for each element defined
+	MRAMConfiguration.Rx0NumElements = 32; //2 aizj		// RX0 Number of elements RemoteFrame <32;STD 64
 	MRAMConfiguration.Rx0ElementSize = MRAM_64_Byte_Data;		// RX0 data payload size
 	MRAMConfiguration.Rx1NumElements = 0;						// RX1 number of elements
 	MRAMConfiguration.Rx1ElementSize = MRAM_64_Byte_Data;		// RX1 data payload size
 	MRAMConfiguration.RxBufNumElements = 64;						// RX buffer number of elements
 	MRAMConfiguration.RxBufElementSize = MRAM_64_Byte_Data;		// RX buffer data payload size
 	MRAMConfiguration.TxEventFIFONumElements = 0;				// TX Event FIFO number of elements
-	MRAMConfiguration.TxBufferNumElements = 2;					// TX buffer number of elements
+	MRAMConfiguration.TxBufferNumElements = 8;//2;					// TX buffer number of elements
 	MRAMConfiguration.TxBufferElementSize = MRAM_64_Byte_Data;	// TX buffer data payload size
 
 
@@ -400,13 +391,16 @@ Init_CAN(void)
 
 
 	/* Store ID 0x12345678 as a priority message */
+/* {0x00258001,0x00258002, 0x00258003, 0x00258004, 0x00198001, 0x00198002, 0x00198003, 0x00198004}*/
+    uint32_t ExFilter[] = {0x00258001,0x00258002, 0x00258003, 0x00258004, 0x00198001, 0x00198002, 0x00198003, 0x00198004};
 	TCAN4x5x_MCAN_XID_Filter XID_ID = {0};
 	XID_ID.EFT = TCAN4x5x_XID_EFT_CLASSIC;                      // EFT
 	XID_ID.EFEC = TCAN4x5x_XID_EFEC_PRIORITYSTORERX0;           // EFEC
-	XID_ID.EFID1 = 0x12345678;                                  // EFID1 (Classic mode filter)
 	XID_ID.EFID2 = 0x1FFFFFFF;                                  // EFID2 (Classic mode mask)
-	TCAN4x5x_MCAN_WriteXIDFilter(0, &XID_ID);                   // Write to the MRAM
-	
+    for(int i=0; i< sizeof(ExFilter)/sizeof(ExFilter[0]);i++){
+	    XID_ID.EFID1 = ExFilter[i];                                  // EFID1 (Classic mode filter)
+        TCAN4x5x_MCAN_WriteXIDFilter(i, &XID_ID);                   // Write to the MRAM    
+	}
 	
 
 	/* Configure the TCAN4550 Non-CAN-related functions */
@@ -517,19 +511,19 @@ Init_GPIO()
 		GPIO_Init(PORT_INT_IN, &GPIO_InitStructure);	
 	
 	/**
-		*	INT-> PB6 PB8 PB9
+		*	INT-> PC4 PC0 PC1
 		*/					 
-		GPIO_InitStructure.GPIO_Pin = INT_IN;
+		GPIO_InitStructure.GPIO_Pin = INT_IN;    //PC4  to TCAN4550 /INT 
 		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
 		GPIO_Init(PORT_INT_IN, &GPIO_InitStructure);	
 	
-		GPIO_InitStructure.GPIO_Pin = INT_TX_IN;
+		GPIO_InitStructure.GPIO_Pin = INT_TX_IN;  //no used
 		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
 		GPIO_Init(PORT_INT_TX_IN, &GPIO_InitStructure);	
 	
-		GPIO_InitStructure.GPIO_Pin = INT_RX_IN;
+		GPIO_InitStructure.GPIO_Pin = INT_RX_IN;  //no used
 		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
 		GPIO_Init(PORT_INT_RX_IN, &GPIO_InitStructure);	
